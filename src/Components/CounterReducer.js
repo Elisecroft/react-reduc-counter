0c225fc3444365b750e905ebeb892a563caf0fc2
Elise Guezel <elise.guezel@yahoo.fr>
2019-12-02 10:12:41 +0100

    Create basic reducer

---
+++ src/Components/CounterReducer.js
@@ -0,0 +1,13 @@
+const CounterReducer = (state = 0, action) => {
+  if (action.type === "ADD-ONE") {
+    return state + 1;
+  } else if (action.type === "ADD-TEN") {
+    return state + 10;
+  } else if (action.type === "DELETE-ONE") {
+    return state - 1;
+  } else if (action.type === "DELETE-TEN") {
+    return state + 10;
+  }
+  return state;
+};
+export default CounterReducer;
