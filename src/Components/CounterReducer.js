b8ed4e279287244197f37bb6b2f0b1826a4bcb65
Elise Guezel <elise.guezel@yahoo.fr>
2019-12-02 10:20:09 +0100

    Add state.json

--- src/Components/CounterReducer.js
+++ src/Components/CounterReducer.js
@@ -6,7 +6,7 @@ const CounterReducer = (state = 0, action) => {
   } else if (action.type === "DELETE-ONE") {
     return state - 1;
   } else if (action.type === "DELETE-TEN") {
-    return state + 10;
+    return state - 10;
   }
   return state;
 };
